dbf30a7fefe1ea07549ba827b10f9d6eff5aae01
EmilieColleye <emilie.colleye@student.hepl.be>
2022-05-01 17:43:41 +0200

    update brick

--- src/ts/Brick.ts
+++ src/ts/Brick.ts
@@ -1,3 +1,24 @@
-export class Brick {
-    
+import {Shape} from "./Shape";
+import {iDrawable} from "./iDrawable";
+
+export class Brick implements iDrawable{
+    private ctx: CanvasRenderingContext2D;
+    private origin:{x: number, y:number}
+    private dimensions:{width: number, height:number}
+    private color: string;
+
+    constructor(ctx : CanvasRenderingContext2D, color: string, origin: { x: number; y: number }, dimensions: { width: number; height: number }) {
+        this.ctx = ctx;
+        this.origin = origin;
+        this.dimensions = dimensions;
+        this.color = color;
+    }
+
+    draw(){
+        this.ctx.beginPath();
+        this.ctx.fillStyle=this.color;
+        this.ctx.rect(this.origin.x, this.origin.y,this.dimensions.width,this.dimensions.height);
+        this.ctx.closePath();
+        this.ctx.fill();
+    }
 }
--- src/ts/Main.ts
+++ src/ts/Main.ts
@@ -3,6 +3,7 @@
 import {iDrawable} from "./iDrawable";
 import {Rectangle} from "./Rectangle";
 import {Ball} from "./Circle";
+import {Brick} from "./Brick";
 
 const canvas: HTMLCanvasElement = document.getElementById('my-canvas') as HTMLCanvasElement;
 const ctx: CanvasRenderingContext2D = canvas.getContext('2d') as CanvasRenderingContext2D;
@@ -24,16 +25,8 @@ let nbLigne = 5;
 let nbColonne = 5;
 let brickWidth = 100;
 let brickHeight = 15;
-let PADDING = 5;
+let padding = 5;
 
-
-//créer un rectangle
-function rect(x: number, y: number, width: number, height: number) {
-    ctx.beginPath();
-    ctx.rect(x,y,width,height);
-    ctx.closePath();
-    ctx.fill();
-}
 
 function initBricks() {
     bricks = new Array(nbLigne);
@@ -45,15 +38,13 @@ function initBricks() {
     }
 }
 
-
 function drawBricks() {
     for (let i=0; i < nbLigne; i++) {
-        ctx.fillStyle = 'blue';
+        //ctx.fillStyle = 'blue';
         for (let j=0; j < nbColonne; j++) {
             if (bricks[i][j] == 1) {
-                rect(canvas.width/3 + (j * (brickWidth + PADDING)) + PADDING,
-                    (i * (brickHeight + PADDING)) + PADDING,
-                    brickWidth, brickHeight);
+                new Brick( ctx, 'blue',{x:canvas.width/3 + (j * (brickWidth + padding)) + padding, y:(i * (brickHeight + padding)) + padding},
+                    {width: brickWidth, height: brickHeight}).draw();
             }
         }
     }
